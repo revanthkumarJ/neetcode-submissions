6d6eb0455241738f4fd9475301e24dbdc47aad1c
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2026-06-02 07:29:32 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int alpha [] = new int[26];
+
+        for(char c:s.toCharArray()){
+            alpha[c-'a']++;
+        }
+
+        for(char c:t.toCharArray()){
+            alpha[c-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(alpha[i]!=0)
+            return false;
+        }
+
+        return true;
+    }
+}
